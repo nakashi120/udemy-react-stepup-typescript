29e8abc39cf1a62b54acab260cf7b2bd3767553a
nakashi120 <nakashi120@gmail.com>
2021-03-10 20:31:24 +0900

    Practice 3

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react"
 import "./App.css"
 import { Practice1 } from "./practicies/Practice1"
 import { Practice2 } from "./practicies/Practice2"
+import { Practice3 } from "./practicies/Practice3"
 
 export default function App() {
   return (
     <div className="App">
       <Practice1 />
       <Practice2 />
+      <Practice3 />
     </div>
   )
 }
