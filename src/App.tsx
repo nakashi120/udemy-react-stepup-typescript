78b8775176e6c30fe0a76a81f0d1c2b8ae36308d
nakashi120 <nakashi120@gmail.com>
2021-03-10 20:56:30 +0900

    Practice 4

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css"
 import { Practice1 } from "./practicies/Practice1"
 import { Practice2 } from "./practicies/Practice2"
 import { Practice3 } from "./practicies/Practice3"
+import { Practice4 } from "./practicies/Practice4"
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Practice1 />
       <Practice2 />
       <Practice3 />
+      <Practice4 />
     </div>
   )
 }
