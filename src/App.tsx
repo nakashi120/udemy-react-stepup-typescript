d92cea53d343b920c1e1b66c55b6db2dd44e238d
nakashi120 <nakashi120@gmail.com>
2021-03-11 00:15:41 +0900

    get data without using custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,50 @@
-import React, { useState } from "react"
 import axios from "axios"
-import { Todo } from "./Todo"
-import { TodoType } from "./types/todo"
-import { Text } from "./Text"
-import { UserProfile } from "./UserProfile"
-import { User } from "./types/user"
-
-const user: User = {
-  name: "nakashi",
-  hobbies: ["映画", "ゲーム"],
-}
+import { useState } from "react"
+import { UserCard } from "./components/UserCard"
+import { User } from "./types/api/user"
+import { UserProfile } from "./types/userProfiles"
 
 export default function App() {
-  const [todos, setTodos] = useState<Array<TodoType>>([])
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
 
-  const onClickFetchData = () => {
+  const onClickFetchUser = () => {
+    setLoading(true)
+    setError(false)
     axios
-      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
-      .then((result) => {
-        setTodos(result.data)
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`,
+        }))
+        setUserProfiles(data)
+      })
+      .catch(() => {
+        setError(true)
+      })
+      .finally(() => {
+        setLoading(false)
       })
   }
-
   return (
     <div className="App">
-      <UserProfile user={user} />
-      <Text color="red" fontSize="18px" />
-      <button onClick={onClickFetchData}>データ取得</button>
-      {todos.map((todo) => (
-        <Todo
-          key={todo.id}
-          title={todo.title}
-          userId={todo.userId}
-          completed={todo.completed}
-        />
-      ))}
+      <button onClick={onClickFetchUser}>データ取得</button>
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}>データの取得に失敗しました。</p>
+      ) : loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </>
+      )}
     </div>
   )
 }
